b551735699d6d1960148a8224678b10ded1ea1ff
Laurent Smet <las@openerp.com>
2017-10-27 12:38:33 +0200

    [FIX] account, l10n_*: preserve existing tags on upgrade
    Taxes tags for some localization have been rewrote in order to improve the taxes report.
    However, these changes delete the existing account.tags on stable version.
    
    These "[l10n_*] one tag per grid in tax reports" should target master instead of 9.0.
    
    -opw: 777464

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -47,6 +47,12 @@ def preserve_existing_tags_on_taxes(cr, registry, module):
     if xml_records:
         cr.execute("update ir_model_data set noupdate = 't' where id in %s", [tuple(xml_records.ids)])
 
+def preserve_existing_tags_on_taxes(cr, registry, module):
+    ''' This is a utility function used to preserve existing previous tags during upgrade of the module.'''
+    xml_record_ids = registry['ir.model.data'].search(cr, SUPERUSER_ID, [('model', 'in', ['account.account.tag']), ('module', 'like', module)])
+    if xml_record_ids:
+        cr.execute("update ir_model_data set noupdate = 't' where id in %s", (tuple(xml_record_ids),))
+
 #  ---------------------------------------------------------------
 #   Account Templates: Account, Tax, Tax Code and chart. + Wizard
 #  ---------------------------------------------------------------
